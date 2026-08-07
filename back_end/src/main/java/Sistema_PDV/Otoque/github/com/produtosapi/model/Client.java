c14e2d3f38b65e2c14a171ac9ae5d69a9fff2dbc
Otoque <nicolastavares198@gmail.com>
2026-08-07 13:02:53 -0300

    mundanças na estrutura do employe e no client

--- back_end/src/main/java/Sistema_PDV/Otoque/github/com/produtosapi/model/Client.java
+++ back_end/src/main/java/Sistema_PDV/Otoque/github/com/produtosapi/model/Client.java
@@ -1,16 +1,55 @@
 package Sistema_PDV.Otoque.github.com.produtosapi.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Client")
 public class Client {
-    
+
     @Id
-    private String cpf;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @Column(unique = true, length = 14)
+    private String cpf;
+
+    @Column(length = 20)
+    private String phone;
+
+    @Column(length = 100)
+    private String email;
+
+    public Client(){}
+
+    public Client(String name, String cpf, String phone, String email){
+        this.name = name;
+        this.cpf = cpf;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getCpf() {
         return cpf;
@@ -18,16 +57,25 @@ public class Client {
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-    public String getName() {
-        return name;
+
+    public String getPhone(){
+        return phone;
     }
-    public void setName(String name) {
-        this.name = name;
+
+    public void setPhone(String phone){
+        this.phone = phone;
     }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
     @Override
     public String toString() {
-        return "Client [cpf=" + cpf + ", name=" + name + "]";
+        return String.format("Client [name: %s, cpf: %s, phone: %s, email: %s]", name, cpf, phone, email);
     }
-    
-
 }
--- back_end/src/main/java/Sistema_PDV/Otoque/github/com/produtosapi/model/Employe.java
+++ back_end/src/main/java/Sistema_PDV/Otoque/github/com/produtosapi/model/Employe.java
@@ -1,6 +1,9 @@
 package Sistema_PDV.Otoque.github.com.produtosapi.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -9,35 +12,70 @@ import jakarta.persistence.Table;
 public class Employe {
 
     @Id
-    private Integer registration;
-    private String position;
-    private String name;
-    private String cpf;
-        
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public Integer getRegistration() {
-        return registration;
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(unique = true, length = 14)
+    private String cpf;
+
+    @Column(length = 50)
+    private String position;
+
+    @Column(unique = true)
+    private Integer registration;
+        
+    public Employe(){}
+
+    public Employe(String name, String cpf, String position, Integer registration){
+        this.name = name;
+        this.cpf = cpf;
+        this.position = position;
+        this.registration = registration;
     }
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-    public void setRegistration(Integer registration) {
-        this.registration = registration;
-    }
+
     public String getPosition() {
         return position;
     }
     public void setPosition(String position) {
         this.position = position;
     }
+
+    public Integer getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(Integer registration) {
+        this.registration = registration;
+    }
     
+    @Override
+    public String toString(){
+        return String.format("Employe [name: %s, cpf: %s, position: %s, registration: %s]", name, cpf, position, registration);
+    }
 }
